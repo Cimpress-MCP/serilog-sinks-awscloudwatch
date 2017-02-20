bc0b255ce61eb89b9e56b43d4234083f40d7ae56
Denys Zhuravel <Denys.Zhuravel@albelli.com>
2017-02-20 14:14:37 +0100

    Added RenderedMessageLogEventRenderer overload which allows using different ITextFormatters

--- src/Serilog.Sinks.AwsCloudWatch/RenderedMessageLogEventRenderer.cs
+++ src/Serilog.Sinks.AwsCloudWatch/RenderedMessageLogEventRenderer.cs
@@ -1,5 +1,6 @@
 using System.IO;
 using Serilog.Events;
+using Serilog.Formatting;
 
 namespace Serilog.Sinks.AwsCloudWatch
 {
@@ -8,13 +9,34 @@ namespace Serilog.Sinks.AwsCloudWatch
     /// </summary>
     public class RenderedMessageLogEventRenderer : ILogEventRenderer
     {
+        private readonly ITextFormatter formatter;
+
+        public RenderedMessageLogEventRenderer()
+            : this(new PlainTextFormatter())
+        {
+
+        }
+
+        public RenderedMessageLogEventRenderer(ITextFormatter formatter)
+        {
+            this.formatter = formatter;
+        }
+
         public string RenderLogEvent(LogEvent logEvent)
         {
             using (var writer = new StringWriter())
             {
-                logEvent.RenderMessage(writer);
+                this.formatter.Format(logEvent, writer);
                 return writer.ToString();
             }
+        }
+    }
+
+    internal sealed class PlainTextFormatter : ITextFormatter
+    {
+        public void Format(LogEvent logEvent, TextWriter output)
+        {
+            logEvent.RenderMessage(output);
         }
     }
 }
