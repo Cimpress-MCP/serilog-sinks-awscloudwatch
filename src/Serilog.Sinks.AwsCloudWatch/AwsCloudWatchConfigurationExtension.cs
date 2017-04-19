5b14bbad25eb6561ea06aff932c77fbd5a8a8419
Christopher Osborn <cosborn@cimpress.com>
2017-04-19 11:54:55 -0400

    Create Overloads To Allow Configuration via AppSettings
    The library "serilog-settings-configuration" allows Serilog to read
    from configuration provided by
    "Microsoft.Extensions.Configuration". This integration has particular
    limitations, and the existing method `AmazonCloudWatch` cannot be
    used.
    
    Three overloads have been created in order to support this:
    
    - One allows the AWS access key, secret access key, and region to be
      read from the environment. Because of this, an implementation of
      `IAmazonCloudWatchLogs` with a default constructor (or a constructor
      with all optional arguments) may be provided via settings. If none
      is provided, an instance of `AmazonCloudWatchLogsClient` is
      created.
    - Another takes in the region from settings, and reads the AWS access
      key and secret access key from the environment. This unconditionally
      creates an instance of `AmazonCloudWatchLogsClient`.
    - Another takes in all values from settings. This also unconditionally
      creates an instance of `AmazonCloudWatchLogsClient`.
    
    In all cases, the log group name is required, and the log event
    renderer, the minimum log event level, the batch size limit, and the
    period are optional, with default values. These default values are the
    same as those used in `CloudWatchSinkOptions`.

--- src/Serilog.Sinks.AwsCloudWatch/AwsCloudWatchConfigurationExtension.cs
+++ src/Serilog.Sinks.AwsCloudWatch/AwsCloudWatchConfigurationExtension.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using Amazon;
 using Amazon.CloudWatchLogs;
 using Serilog.Configuration;
 using Serilog.Core;
 using Serilog.Events;
+using static Serilog.Sinks.AwsCloudWatch.CloudWatchSinkOptions;
 
 namespace Serilog.Sinks.AwsCloudWatch
 {
@@ -25,9 +27,130 @@ namespace Serilog.Sinks.AwsCloudWatch
 
             // create the sink
             ILogEventSink sink = new CloudWatchLogSink(cloudWatchClient, options);
-            
+
             // register the sink
             return loggerConfiguration.Sink(sink, options.MinimumLogEventLevel);
+        }
+
+        /// <summary>
+        /// Activates logging to AWS CloudWatch
+        /// </summary>
+        /// <remarks>This overload is intended to be used via AppSettings integration.</remarks>
+        /// <param name="loggerConfiguration">The LoggerSinkConfiguration to register this sink with.</param>
+        /// <param name="logGroupName">The log group name to be used in AWS CloudWatch.</param>
+        /// <param name="cloudWatchClient">An AWS CloudWatch client which includes access to AWS and AWS specific settings like the AWS region.</param>
+        /// <param name="logEventRenderer">A renderer to render Serilog's LogEvent.</param>
+        /// <param name="minimumLogEventLevel">The minimum log event level required in order to write an event to the sink.</param>
+        /// <param name="batchSizeLimit">The batch size to be used when uploading logs to AWS CloudWatch.</param>
+        /// <param name="period">The period to be used when a batch upload should be triggered.</param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"><paramref name="logGroupName"/> is <see langword="null"/>.</exception>
+        public static LoggerConfiguration AmazonCloudWatch(
+            this LoggerSinkConfiguration loggerConfiguration,
+            string logGroupName,
+            IAmazonCloudWatchLogs cloudWatchClient = null,
+            ILogEventRenderer logEventRenderer = null,
+            LogEventLevel minimumLogEventLevel = DefaultMinimumLogEventLevel,
+            int batchSizeLimit = DefaultBatchSizeLimit,
+            TimeSpan? period = null)
+        {
+            if (logGroupName == null) throw new ArgumentNullException(nameof(logGroupName));
+
+            var options = new CloudWatchSinkOptions
+            {
+                LogGroupName = logGroupName,
+                MinimumLogEventLevel = minimumLogEventLevel,
+                BatchSizeLimit = batchSizeLimit,
+                Period = period ?? DefaultPeriod,
+                LogEventRenderer = logEventRenderer
+            };
+            var client = cloudWatchClient ?? new AmazonCloudWatchLogsClient();
+            return loggerConfiguration.AmazonCloudWatch(options, client);
+        }
+
+        /// <summary>
+        /// Activates logging to AWS CloudWatch
+        /// </summary>
+        /// <remarks>This overload is intended to be used via AppSettings integration.</remarks>
+        /// <param name="loggerConfiguration">The LoggerSinkConfiguration to register this sink with.</param>
+        /// <param name="logGroupName">The log group name to be used in AWS CloudWatch.</param>
+        /// <param name="regionName">The system name of the region to which to write.</param>
+        /// <param name="logEventRenderer">A renderer to render Serilog's LogEvent.</param>
+        /// <param name="minimumLogEventLevel">The minimum log event level required in order to write an event to the sink.</param>
+        /// <param name="batchSizeLimit">The batch size to be used when uploading logs to AWS CloudWatch.</param>
+        /// <param name="period">The period to be used when a batch upload should be triggered.</param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"><paramref name="logGroupName"/> is <see langword="null"/>.</exception>
+        /// <exception cref="ArgumentNullException"><paramref name="regionName"/> is <see langword="null"/>.</exception>
+        public static LoggerConfiguration AmazonCloudWatch(
+            this LoggerSinkConfiguration loggerConfiguration,
+            string logGroupName,
+            string regionName,
+            ILogEventRenderer logEventRenderer = null,
+            LogEventLevel minimumLogEventLevel = DefaultMinimumLogEventLevel,
+            int batchSizeLimit = DefaultBatchSizeLimit,
+            TimeSpan? period = null)
+        {
+            if (logGroupName == null) throw new ArgumentNullException(nameof(logGroupName));
+            if (regionName == null) throw new ArgumentNullException(nameof(regionName));
+
+            var options = new CloudWatchSinkOptions
+            {
+                LogGroupName = logGroupName,
+                MinimumLogEventLevel = minimumLogEventLevel,
+                BatchSizeLimit = batchSizeLimit,
+                Period = period ?? DefaultPeriod,
+                LogEventRenderer = logEventRenderer
+            };
+            var client = new AmazonCloudWatchLogsClient(RegionEndpoint.GetBySystemName(regionName));
+            return loggerConfiguration.AmazonCloudWatch(options, client);
+        }
+
+        /// <summary>
+        /// Activates logging to AWS CloudWatch
+        /// </summary>
+        /// <remarks>This overload is intended to be used via AppSettings integration.</remarks>
+        /// <param name="loggerConfiguration">The LoggerSinkConfiguration to register this sink with.</param>
+        /// <param name="logGroupName">The log group name to be used in AWS CloudWatch.</param>
+        /// <param name="accessKey">The access key to use to access AWS CloudWatch.</param>
+        /// <param name="secretAccessKey">The secret access key to use to access AWS CloudWatch.</param>
+        /// <param name="regionName">The system name of the region to which to write.</param>
+        /// <param name="logEventRenderer">A renderer to render Serilog's LogEvent.</param>
+        /// <param name="minimumLogEventLevel">The minimum log event level required in order to write an event to the sink.</param>
+        /// <param name="batchSizeLimit">The batch size to be used when uploading logs to AWS CloudWatch.</param>
+        /// <param name="period">The period to be used when a batch upload should be triggered.</param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"><paramref name="logGroupName"/> is <see langword="null"/>.</exception>
+        /// <exception cref="ArgumentNullException"><paramref name="accessKey"/> is <see langword="null"/>.</exception>
+        /// <exception cref="ArgumentNullException"><paramref name="secretAccessKey"/> is <see langword="null"/>.</exception>
+        /// <exception cref="ArgumentNullException"><paramref name="regionName"/> is <see langword="null"/>.</exception>
+        public static LoggerConfiguration AmazonCloudWatch(
+            this LoggerSinkConfiguration loggerConfiguration,
+            string logGroupName,
+            string accessKey,
+            string secretAccessKey,
+            string regionName,
+            ILogEventRenderer logEventRenderer = null,
+            LogEventLevel minimumLogEventLevel = DefaultMinimumLogEventLevel,
+            int batchSizeLimit = DefaultBatchSizeLimit,
+            TimeSpan? period = null)
+        {
+            if (logGroupName == null) throw new ArgumentNullException(nameof(logGroupName));
+            if (accessKey == null) throw new ArgumentNullException(nameof(accessKey));
+            if (secretAccessKey == null) throw new ArgumentNullException(nameof(secretAccessKey));
+            if (regionName == null) throw new ArgumentNullException(nameof(regionName));
+
+            var options = new CloudWatchSinkOptions
+            {
+                LogGroupName = logGroupName,
+                MinimumLogEventLevel = minimumLogEventLevel,
+                BatchSizeLimit = batchSizeLimit,
+                Period = period ?? DefaultPeriod,
+                LogEventRenderer = logEventRenderer
+            };
+            var client = new AmazonCloudWatchLogsClient(accessKey, secretAccessKey,
+                RegionEndpoint.GetBySystemName(regionName));
+            return loggerConfiguration.AmazonCloudWatch(options, client);
         }
     }
 }
