6ff15f0473e2314b31e0afb80262de18baa56492
Travis Schlotfeldt <TSchlotfeldt@balanceinnovations.com>
2017-10-30 11:25:43 -0500

    positive cases handled

--- src/Serilog.Sinks.AwsCloudWatch/CloudWatchLogSink.cs
+++ src/Serilog.Sinks.AwsCloudWatch/CloudWatchLogSink.cs
@@ -1,11 +1,11 @@
 using Amazon.CloudWatchLogs;
+using Amazon.CloudWatchLogs.Model;
+using Serilog.Events;
 using Serilog.Sinks.PeriodicBatching;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Serilog.Events;
-using Amazon.CloudWatchLogs.Model;
 
 namespace Serilog.Sinks.AwsCloudWatch
 {
@@ -20,11 +20,21 @@ namespace Serilog.Sinks.AwsCloudWatch
         /// The maximum log event batch size = 1 MB
         /// </summary>
         public const int MaxLogEventBatchSize = 1048576;
-        
+
         /// <summary>
         /// The maximum log event batch count
         /// </summary>
         public const int MaxLogEventBatchCount = 10000;
+
+        /// <summary>
+        /// The maximum span of events in a batch
+        /// </summary>
+        public static readonly TimeSpan MaxBatchEventSpan = TimeSpan.FromDays(1);
+
+        /// <summary>
+        /// The span of time to throttle requests at
+        /// </summary>
+        public static readonly TimeSpan ThrottlingInterval = TimeSpan.FromMilliseconds(200);
 
         private readonly IAmazonCloudWatchLogs cloudWatchClient;
         private readonly CloudWatchSinkOptions options;
@@ -33,7 +43,7 @@ namespace Serilog.Sinks.AwsCloudWatch
         private string nextSequenceToken;
         private readonly ILogEventRenderer renderer;
 
-        public CloudWatchLogSink(IAmazonCloudWatchLogs cloudWatchClient, CloudWatchSinkOptions options) 
+        public CloudWatchLogSink(IAmazonCloudWatchLogs cloudWatchClient, CloudWatchSinkOptions options)
             : base(options.BatchSizeLimit, options.Period)
         {
             if (options.BatchSizeLimit < 1)
@@ -51,6 +61,11 @@ namespace Serilog.Sinks.AwsCloudWatch
 
         protected override async Task EmitBatchAsync(IEnumerable<LogEvent> events)
         {
+            if (events.Count() == 0)
+            {
+                return;
+            }
+
             // We do not need synchronization in this method since it is only called from a single thread by the PeriodicBatchSink.
 
             try
@@ -65,21 +80,74 @@ namespace Serilog.Sinks.AwsCloudWatch
 
             try
             {
-                // log events need to be ordered by timestamp within a single bulk upload to CloudWatch
                 var logEvents =
-                    events.OrderBy(e => e.Timestamp)
-                        .Select(e =>
-                        {
-                            var message = renderer.RenderLogEvent(e);
-                            return new InputLogEvent
+                    new Queue<InputLogEvent>(events
+                        .OrderBy(e => e.Timestamp) // log events need to be ordered by timestamp within a single bulk upload to CloudWatch
+                        .Select( // transform
+                            @event =>
                             {
-                                Message = message.Substring(0, Math.Min(message.Length, MaxLogEventSize)),
-                                //Message = message,
-                                Timestamp = e.Timestamp.UtcDateTime
-                            };
-                        })
-                        .ToList();
-                //.GroupBy(item => item.Timestamp.Date); // ensures a batch will not span more than 24 hours
+                                var message = renderer.RenderLogEvent(@event);
+                                if (message.Length > MaxLogEventSize)
+                                {
+                                    Debugging.SelfLog.WriteLine("Truncating log event with length of {0}", message.Length);
+                                    message = message.Substring(0, MaxLogEventSize);
+                                }
+                                return new InputLogEvent
+                                {
+                                    Message = message,
+                                    Timestamp = @event.Timestamp.UtcDateTime
+                                };
+                            }));
+
+                while (logEvents.Count > 0)
+                {
+                    DateTime? first = null;
+                    var batchSize = 0;
+                    var batch = new List<InputLogEvent>();
+
+                    do
+                    {
+                        var @event = logEvents.Peek();
+                        if (!first.HasValue)
+                        {
+                            first = @event.Timestamp;
+                        }
+                        else if (@event.Timestamp.Subtract(first.Value) > MaxBatchEventSpan) // ensure batch spans no more than 24 hours
+                        {
+                            break;
+                        }
+
+                        if (batchSize + @event.Message.Length < MaxLogEventBatchSize) // ensure < max batch size
+                        {
+                            batchSize += @event.Message.Length;
+                            batch.Add(@event);
+                            logEvents.Dequeue();
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    } while (batch.Count < MaxLogEventBatchCount && logEvents.Count > 0); // ensure < max batch count
+
+                    // creates the request to upload a new event to CloudWatch
+                    PutLogEventsRequest putEventsRequest = new PutLogEventsRequest
+                    {
+                        LogGroupName = options.LogGroupName,
+                        LogStreamName = logStreamName,
+                        SequenceToken = nextSequenceToken,
+                        LogEvents = batch
+                    };
+
+                    // actually upload the event to CloudWatch
+                    var putLogEventsResponse = await cloudWatchClient.PutLogEventsAsync(putEventsRequest);
+
+                    // remember the next sequence token, which is required
+                    nextSequenceToken = putLogEventsResponse.NextSequenceToken;
+
+                    // throttle
+                    await Task.Delay(ThrottlingInterval);
+                }
+
 
                 //logEvents.TakeWhile(@event =>
                 //{
@@ -93,18 +161,17 @@ namespace Serilog.Sinks.AwsCloudWatch
 
                 //} while (attempt < options.RetryAttempts);
 
-                // creates the request to upload a new event to CloudWatch
-                PutLogEventsRequest putEventsRequest = new PutLogEventsRequest(options.LogGroupName, logStreamName, logEvents)
-                {
-                    SequenceToken = nextSequenceToken
-                };
-
-                // actually upload the event to CloudWatch
-                var putLogEventsResponse = await cloudWatchClient.PutLogEventsAsync(putEventsRequest);
             }
             catch (Exception ex)
             {
-                Console.WriteLine("bad, bad, bad... {0}", ex);
+                try
+                {
+                    Debugging.SelfLog.WriteLine("Error sending logs. No logs will be sent to AWS CloudWatch. Error was {0}", ex);
+                }
+                catch (Exception)
+                {
+                    // we even failed to log to the trace logger - giving up trying to put something out
+                }
             }
         }
     }
--- test/Serilog.Sinks.AwsCloudWatch.Tests/CloudWatchLogsSinkTests.cs
+++ test/Serilog.Sinks.AwsCloudWatch.Tests/CloudWatchLogsSinkTests.cs
@@ -14,50 +14,309 @@ namespace Serilog.Sinks.AwsCloudWatch.Tests
 {
     public class CloudWatchLogsSinkTests
     {
-        public async Task HappyPath()
-        {
+        private const string Alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        private static Random random = new Random((int)DateTime.Now.Ticks);
 
-        }
-
-        [Fact(DisplayName = "EmitBatchAsync - Truncate message")]
-        public async Task LargeMessage_Truncated()
+        [Fact(DisplayName = "EmitBatchAsync - Single batch")]
+        public async Task SingleBatch()
         {
-            var client = new Mock<IAmazonCloudWatchLogs>();
+            // expect a single batch of events to be posted to CloudWatch Logs
+
+            var client = new Mock<IAmazonCloudWatchLogs>(MockBehavior.Strict);
             var options = new CloudWatchSinkOptions();
             var sink = new CloudWatchLogSink(client.Object, options);
-            var largeEventMessage = new string(Enumerable.Repeat('a', CloudWatchLogSink.MaxLogEventSize + 1).ToArray());
+            var events = Enumerable.Range(0, 10)
+                .Select(_ => // create 10 events with message length of 12
+                    new LogEvent(
+                        DateTimeOffset.UtcNow,
+                        LogEventLevel.Information,
+                        null,
+                        new MessageTemplateParser().Parse(CreateMessage(12)),
+                        Enumerable.Empty<LogEventProperty>()))
+                .ToArray();
+
+            var putLogEventsCalls = new List<(PutLogEventsRequest request, CancellationToken cancellationToken)>();
+            client.Setup(mock => mock.PutLogEventsAsync(It.IsAny<PutLogEventsRequest>(), It.IsAny<CancellationToken>()))
+                .Callback<PutLogEventsRequest, CancellationToken>((putLogEventsRequest, cancellationToken) => putLogEventsCalls.Add((putLogEventsRequest, cancellationToken))); // keep track of the requests made
+
+            await sink.EmitBatchAsync(events);
+
+            Assert.Single(putLogEventsCalls);
+
+            var request = putLogEventsCalls.First().request;
+            Assert.Equal(options.LogGroupName, request.LogGroupName);
+            Assert.Null(request.SequenceToken);
+            Assert.Equal(10, request.LogEvents.Count);
+            for (var i = 0; i < events.Length; i++)
+            {
+                Assert.Equal(events[i].MessageTemplate.Text, request.LogEvents.ElementAt(i).Message);
+            }
+        }
+
+        [Fact(DisplayName = "EmitBatchAsync - Large message")]
+        public async Task LargeMessage()
+        {
+            // expect an event with a length beyond the MaxLogEventSize will be truncated to the MaxLogEventSize
+
+            var client = new Mock<IAmazonCloudWatchLogs>(MockBehavior.Strict);
+            var options = new CloudWatchSinkOptions();
+            var sink = new CloudWatchLogSink(client.Object, options);
+            var largeEventMessage = CreateMessage(CloudWatchLogSink.MaxLogEventSize + 1);
             var events = new LogEvent[]
             {
                 new LogEvent(
-                    DateTimeOffset.UtcNow, 
-                    LogEventLevel.Information, 
+                    DateTimeOffset.UtcNow,
+                    LogEventLevel.Information,
                     null,
                     new MessageTemplateParser().Parse(largeEventMessage),
                     Enumerable.Empty<LogEventProperty>())
             };
 
-            var putLogEventsRequests = new List<PutLogEventsRequest>();
+            var putLogEventsCalls = new List<(PutLogEventsRequest request, CancellationToken cancellationToken)>();
             client.Setup(mock => mock.PutLogEventsAsync(It.IsAny<PutLogEventsRequest>(), It.IsAny<CancellationToken>()))
-                .Callback<PutLogEventsRequest, CancellationToken>((putLogEventsRequest, cancellationToken) => putLogEventsRequests.Add(putLogEventsRequest)); // keep track of the requests made
+                .Callback<PutLogEventsRequest, CancellationToken>((putLogEventsRequest, cancellationToken) => putLogEventsCalls.Add((putLogEventsRequest, cancellationToken))); // keep track of the requests made
 
             await sink.EmitBatchAsync(events);
 
-            Assert.Single(putLogEventsRequests);
+            Assert.Single(putLogEventsCalls);
 
-            var request = putLogEventsRequests.First();
+            var request = putLogEventsCalls.First().request;
             Assert.Equal(options.LogGroupName, request.LogGroupName);
+            Assert.Null(request.SequenceToken);
+            Assert.Single(request.LogEvents);
+            Assert.Equal(largeEventMessage.Substring(0, CloudWatchLogSink.MaxLogEventSize), request.LogEvents.First().Message);
+        }
 
-            //client.Verify(
-            //    mock => mock.PutLogEventsAsync(
-            //        It.Is<PutLogEventsRequest>(request =>
-            //            request.LogGroupName == options.LogGroupName
-            //            && request.SequenceToken == null
-            //            && request.LogEvents.Count == 1
-            //            && request.LogEvents.First().Message == largeEventMessage.Substring(0, CloudWatchLogSink.MaxLogEventSize)),
-            //        It.IsAny<CancellationToken>()), 
-            //    Times.Once);
+        [Fact(DisplayName = "EmitBatchAsync - Beyond batch span")]
+        public async Task MultipleDays()
+        {
+            // expect a batch to be posted for each 24-hour period
+
+            var client = new Mock<IAmazonCloudWatchLogs>(MockBehavior.Strict);
+            var options = new CloudWatchSinkOptions();
+            var sink = new CloudWatchLogSink(client.Object, options);
+            var events = Enumerable.Range(0, 20)
+                .Select(i => // create multipe events with message length of 12
+                    new LogEvent(
+                        DateTimeOffset.UtcNow.Subtract(TimeSpan.FromDays((i % 2) * 2)), // split the events into two days
+                        LogEventLevel.Information,
+                        null,
+                        new MessageTemplateParser().Parse(CreateMessage(12)),
+                        Enumerable.Empty<LogEventProperty>()))
+                .ToArray();
+
+            var putLogEventsCalls = new List<(PutLogEventsRequest request, CancellationToken cancellationToken, DateTime datetime)>();
+            client.Setup(mock => mock.PutLogEventsAsync(It.IsAny<PutLogEventsRequest>(), It.IsAny<CancellationToken>()))
+                .Callback<PutLogEventsRequest, CancellationToken>((putLogEventsRequest, cancellationToken) => putLogEventsCalls.Add((putLogEventsRequest, cancellationToken, DateTime.UtcNow))) // keep track of the requests made
+                .ReturnsAsync(new PutLogEventsResponse
+                {
+                    HttpStatusCode = System.Net.HttpStatusCode.OK,
+                    NextSequenceToken = Guid.NewGuid().ToString()
+                });
+
+            await sink.EmitBatchAsync(events);
+
+            Assert.Equal(2, putLogEventsCalls.Count);
+
+            for (var i = 0; i < putLogEventsCalls.Count; i++)
+            {
+                var call = putLogEventsCalls[i];
+                var request = call.request;
+
+                Assert.Equal(options.LogGroupName, request.LogGroupName);
+                Assert.Equal(events.Length / putLogEventsCalls.Count, request.LogEvents.Count);
+
+                // make sure the events are ordered
+                for (var index = 1; index < call.request.LogEvents.Count; index++)
+                {
+                    Assert.True(call.request.LogEvents.ElementAt(index).Timestamp > call.request.LogEvents.ElementAt(index - 1).Timestamp);
+                }
+
+                if (i == 0) // first call
+                {
+                    Assert.Null(request.SequenceToken);
+                }
+                else
+                {
+                    Assert.NotNull(request.SequenceToken);
+
+                    // ensure calls are throttled
+                    var interval = call.datetime.Subtract(putLogEventsCalls[i - 1].datetime);
+                    Assert.True(interval >= CloudWatchLogSink.ThrottlingInterval);
+                }
+            }
+        }
+
+        [Fact(DisplayName = "EmitBatchAsync - Beyond max batch count")]
+        public async Task MoreThanMaxBatchCount()
+        {
+            // expect multiple batches, all having a batch count less than the maximum
+
+            var client = new Mock<IAmazonCloudWatchLogs>(MockBehavior.Strict);
+            var options = new CloudWatchSinkOptions();
+            var sink = new CloudWatchLogSink(client.Object, options);
+            var events = Enumerable.Range(0, CloudWatchLogSink.MaxLogEventBatchCount + 1)
+                .Select(i => 
+                    new LogEvent(
+                        DateTimeOffset.UtcNow,
+                        LogEventLevel.Information,
+                        null,
+                        new MessageTemplateParser().Parse(CreateMessage(2)), // make sure size is not an issue
+                        Enumerable.Empty<LogEventProperty>()))
+                .ToArray();
+
+            var putLogEventsCalls = new List<(PutLogEventsRequest request, CancellationToken cancellationToken, DateTime datetime)>();
+            client.Setup(mock => mock.PutLogEventsAsync(It.IsAny<PutLogEventsRequest>(), It.IsAny<CancellationToken>()))
+                .Callback<PutLogEventsRequest, CancellationToken>((putLogEventsRequest, cancellationToken) => putLogEventsCalls.Add((putLogEventsRequest, cancellationToken, DateTime.UtcNow))) // keep track of the requests made
+                .ReturnsAsync(new PutLogEventsResponse
+                {
+                    HttpStatusCode = System.Net.HttpStatusCode.OK,
+                    NextSequenceToken = Guid.NewGuid().ToString()
+                });
+
+            await sink.EmitBatchAsync(events);
+
+            Assert.Equal(2, putLogEventsCalls.Count);
+
+            for (var i = 0; i < putLogEventsCalls.Count; i++)
+            {
+                var call = putLogEventsCalls[i];
+                var request = call.request;
+
+                Assert.Equal(options.LogGroupName, request.LogGroupName);
+
+                if (i == 0) // first call
+                {
+                    Assert.Null(request.SequenceToken);
+                    Assert.Equal(CloudWatchLogSink.MaxLogEventBatchCount, request.LogEvents.Count);
+                }
+                else
+                {
+                    Assert.NotNull(request.SequenceToken);
+                    Assert.Single(request.LogEvents);
+
+                    // ensure calls are throttled
+                    var interval = call.datetime.Subtract(putLogEventsCalls[i - 1].datetime);
+                    Assert.True(interval >= CloudWatchLogSink.ThrottlingInterval);
+                }
+            }
+        }
+
+        [Fact(DisplayName = "EmitBatchAsync - Beyond batch size")]
+        public async Task MoreThanMaxBatchSize()
+        {
+            // expect multiple batches, all having a batch size less than the maximum
+
+            var client = new Mock<IAmazonCloudWatchLogs>(MockBehavior.Strict);
+            var options = new CloudWatchSinkOptions();
+            var sink = new CloudWatchLogSink(client.Object, options);
+            var events = Enumerable.Range(0, 256) // 256 4 KB messages matches our max batch size, but we want to test a "less nice" scenario, so we'll create 256 5 KB messages
+                .Select(i =>
+                    new LogEvent(
+                        DateTimeOffset.UtcNow,
+                        LogEventLevel.Information,
+                        null,
+                        new MessageTemplateParser().Parse(CreateMessage(1024 * 5)), // 5 KB messages
+                        Enumerable.Empty<LogEventProperty>()))
+                .ToArray();
+
+            var putLogEventsCalls = new List<(PutLogEventsRequest request, CancellationToken cancellationToken, DateTime datetime)>();
+            client.Setup(mock => mock.PutLogEventsAsync(It.IsAny<PutLogEventsRequest>(), It.IsAny<CancellationToken>()))
+                .Callback<PutLogEventsRequest, CancellationToken>((putLogEventsRequest, cancellationToken) => putLogEventsCalls.Add((putLogEventsRequest, cancellationToken, DateTime.UtcNow))) // keep track of the requests made
+                .ReturnsAsync(new PutLogEventsResponse
+                {
+                    HttpStatusCode = System.Net.HttpStatusCode.OK,
+                    NextSequenceToken = Guid.NewGuid().ToString()
+                });
+
+            await sink.EmitBatchAsync(events);
+
+            Assert.Equal(2, putLogEventsCalls.Count);
+
+            for (var i = 0; i < putLogEventsCalls.Count; i++)
+            {
+                var call = putLogEventsCalls[i];
+                var request = call.request;
+
+                Assert.Equal(options.LogGroupName, request.LogGroupName);
+
+                if (i == 0) // first call
+                {
+                    Assert.Null(request.SequenceToken);
+                    Assert.Equal(204, request.LogEvents.Count); // expect 204 of the 256 messages in the first batch
+                }
+                else
+                {
+                    Assert.NotNull(request.SequenceToken);
+                    Assert.Equal(52, request.LogEvents.Count); // expect 52 of the 256 messages in the second batch
+
+                    // ensure calls are throttled
+                    var interval = call.datetime.Subtract(putLogEventsCalls[i - 1].datetime);
+                    Assert.True(interval >= CloudWatchLogSink.ThrottlingInterval);
+                }
+            }
+        }
+
+        [Fact(DisplayName = "EmitBatchAsync - Service unavailable", Skip = "Implement")]
+        public async Task ServiceUnavailable()
+        {
+            // expect retries until exhausted
+        }
+
+        [Fact(DisplayName = "EmitBatchAsync - Service unavailable with eventual success", Skip = "Implement")]
+        public async Task ServiceUnavailable_WithEventualSuccess()
+        {
+            // expect successful posting of batch after retry
+        }
+
+        [Fact(DisplayName = "EmitBatchAsync - Resource not found", Skip = "Implement")]
+        public async Task ResourceNotFound()
+        {
+            // expect failure, creation of log group/stream, and evenutal success
+        }
+
+        [Fact(DisplayName = "EmitBatchAsync - Unable to create resource", Skip = "Implement")]
+        public async Task ResourceNotFound_CannotCreateResource()
+        {
+            // expect failure with failure to successfully create resources upon retries
+        }
+
+        [Fact(DisplayName = "EmitBatchAsync - Invalid parameter", Skip = "Implement")]
+        public async Task InvalidParameter()
+        {
+            // expect batch dropped
+        }
+
+        [Fact(DisplayName = "EmitBatchAsync - Invalid sequence token", Skip = "Implement")]
+        public async Task InvalidSequenceToken()
+        {
+            // expect update of sequence token and successful retry
+        }
+
+        [Fact(DisplayName = "EmitBatchAsync - Invalid sequence token with new log stream", Skip = "Implement")]
+        public async Task InvalidSequenceToken_CannotUpdateSequenceToken()
+        {
+            // expect update of sequence token and successful retry
+        }
+
+        [Fact(DisplayName = "EmitBatchAsync - Data already accepted", Skip = "Implement")]
+        public async Task DataAlreadyAccepted()
+        {
+            // expect update of sequence token and successful retry
         }
 
 
+
+        /// <summary>
+        /// Creates a message of random characters of the given size.
+        /// </summary>
+        /// <param name="size">The size of the message.</param>
+        /// <returns>A string consisting of random characters from the alphabet.</returns>
+        private string CreateMessage(int size)
+        {
+            var message = new string(Enumerable.Range(0, size).Select(_ => Alphabet[random.Next(0, Alphabet.Length)]).ToArray());
+            Assert.Equal(size, message.Length);
+            return message;
+        }
     }
 }
